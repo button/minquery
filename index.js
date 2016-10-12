45045285b27427c85bfc7c95f1edcd1e84d5410e
mike wakerly <mikey@usebutton.com>
2016-10-12 19:37:01 -0400

    Super cool tests!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const crypto = require('crypto');
+const fs = require('fs');
 const request = require('google-oauth-jwt').requestWithJWT();
 
 const SCOPES = [
@@ -8,6 +8,7 @@ const SCOPES = [
   'https://www.googleapis.com/auth/bigquery.insertdata'
 ];
 
+/** Base error class. */
 class MinQueryError extends Error {
   constructor(message) {
     super(message);
@@ -35,16 +36,49 @@ class ProtocolError extends ResponseError {
 
 class MinQuery {
 
-  constructor(keyFile, email, projectId ) {
-    this.keyFile = keyFile;
-    this.email = email;
-    this.projectId = projectId;
+  /**
+   * Constructor.
+   *
+   * @param  {string} options.key       A PEM-encoded private key to authenticate
+   *                                    to the BigQuery API. Must be specified if
+   *                                   `options.keyFile` is not specified.
+   * @param  {string} options.keyFile   Path to a PEM-encoded private key,
+   *                                    which will be read synchronously in
+   *                                    the constructor. Must be specified if
+   *                                    `options.key` is not specified.
+   * @param  {string} options.email     Google auth e-mail address. Required.
+   * @param  {string} options.projectId Google cloud project ID. Required.
+   */
+  constructor(options) {
+    options = options || {};
+
+    if (!options.key && !options.keyFile) {
+      throw new Error('Must specify `options.key` or `options.keyFile`');
+    } else if (options.key && options.keyFile) {
+      throw new Error('Specify only one of `options.key` or `options.keyFile`');
+    }
+
+    if (!options.email) {
+      throw new Error('Must specify `options.email`');
+    }
+    if (!options.projectId) {
+      throw new Error('Must specify `options.projectId`');
+    }
+
+    if (options.keyFile) {
+      this.key = fs.readFileSync(options.keyFile);
+    } else {
+      this.key = options.key;
+    }
+
+    this.email = options.email;
+    this.projectId = options.projectId;
   }
 
   _request(method, path, data) {
     const url = `https://www.googleapis.com/bigquery/v2/projects/${this.projectId}${path}`;
 
-    const promise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       request({
         method: method,
         body: data,
@@ -52,7 +86,7 @@ class MinQuery {
         url: url,
         jwt: {
           email: this.email,
-          keyFile: this.keyFile,
+          key: this.key,
           scopes: SCOPES
         }
       }, (err, res) => {
@@ -67,7 +101,6 @@ class MinQuery {
         }
       });
     });
-    return promise;
   }
 
   /**
@@ -76,11 +109,14 @@ class MinQuery {
    * Ref: https://cloud.google.com/bigquery/docs/reference/v2/tables/insert
    * Ref: https://cloud.google.com/bigquery/docs/reference/v2/tables#resource
    *
-   * @param  {string} dataset                 dataset name
-   * @param  {string} tableName               table name
-   * @param  {object[]} fields                table schema
-   * @param  {Date}   options.expirationDate  If set, a Date object specifying when
-   *                                          the table should be expired.
+   * @param  {string} dataset                  dataset name
+   * @param  {string} tableName                table name
+   * @param  {object[]} fields                 table schema
+   * @param  {Date}   options.expirationDate   If set, a Date object specifying when
+   *                                           the table should be expired.
+   * @param  {object} options.timePartitioning If set, an object containing fields
+   *                                           `expirationMs` and `type` which will be
+   *                                           passed through to the BigQuery API.
    */
   createTable(dataset, tableName, fields, options) {
     options = options || {};
@@ -112,19 +148,30 @@ class MinQuery {
    *
    * Ref: https://cloud.google.com/bigquery/docs/reference/v2/tabledata/insertAll
    *
-   * @param  {string} dataset     dataset name
-   * @param  {string} tableName   table name
-   * @param  {object[]} rows      row data
+   * @param  {string}   dataset                     dataset name
+   * @param  {string}   tableName                   table name
+   * @param  {object[]} rows                        row data
+   * @param  {boolean}  options.skipInvalidRows     Passed through to BigQuery API:
+   *                                                Don't fail the whole request if a
+   *                                                row is invalid (default true).
+   * @param  {boolean}  options.ignoreUnknownValues Passed through to BigQuery API:
+   *                                                Don't fail the whole request if a
+   *                                                row is invalid (default true).
+   * @param  {boolean}  options.addInsertId         Add a random insert id to each
    */
-  insert(dataset, tableName, rows) {
+  insert(dataset, tableName, rows, options) {
+    options = options || {};
+
     const rowData = rows.map((row) => {
-      let insertId = crypto.randomBytes(16).toString('hex');
-      return { insertId: insertId, json: row };
+      return { json: row };
     });
+
     const data = {
       kind: 'bigquery#tableDataInsertAllRequest',
-      skipInvalidRows: true,
-      ignoreUnknownValues: true,
+      skipInvalidRows: options.skipInvalidRows !== undefined ?
+        !!options.skipInvalidRows : true,
+      ignoreUnknownValues: options.ignoreUnknownValues !== undefined ?
+        !!options.ignoreUnknownValues : true,
       rows: rowData
     };
     return this._request('POST', `/datasets/${dataset}/tables/${tableName}/insertAll`, data);
---
+++ tests/api-test.js
@@ -0,0 +1,254 @@
+'use strict';
+
+const assert = require('assert');
+const nock = require('nock');
+const MinQuery = require('../index');
+
+describe('minquery', () => {
+
+  describe('#constructor', () => {
+
+    it('fails when key and keyFile are missing', () => {
+      assert.throws(() => new MinQuery(),
+        /Must specify `options.key` or `options.keyFile`/);
+    });
+
+    it('fails when both key and keyFile are given', () => {
+      assert.throws(() => new MinQuery({
+        key: 'fake-key',
+        keyFile: 'fake-key-file.txt'
+      }), /Specify only one of `options.key` or `options.keyFile`/);
+    });
+
+    it('fails when email is missing', () => {
+      assert.throws(() => new MinQuery({
+        key: 'fake-key',
+        projectId: 'fake-project'
+      }), /Must specify `options.email`/);
+    });
+
+    it('fails when projectId is missing', () => {
+      assert.throws(() => new MinQuery({
+        key: 'fake-key',
+        email: 'fake-email'
+      }), /Must specify `options.projectId`/);
+    });
+
+    it('succeeds when all required fields are given (key)', () => {
+      new MinQuery({
+        key: 'fake-key',
+        email: 'fake-email',
+        projectId: 'fake-project'
+      });
+    });
+
+    it('succeeds when all required fields are given (keyFile)', () => {
+      new MinQuery({
+        keyFile: __dirname + '/fixtures/fake-key.pem',
+        email: 'fake-email',
+        projectId: 'fake-project'
+      });
+    });
+
+  });
+
+  describe('#createTable', () => {
+
+    let client;
+    let nockBack;
+
+    const ignoreOauthNockOpts = {
+      before: (scope) => {
+        // Because google-oauth-jwt is going to create a unique request
+        // body every time we test, patch nock to treat oauth-looking
+        // requests as the literal body '*'. Fixtures have been patched to
+        // match on that as well.
+        scope.filteringRequestBody = (body) => {
+          if (/grant_type=.*assertion=.*/.test(body)) {
+            return '*';
+          }
+          return body;
+        };
+      }
+    };
+
+    beforeEach(() => {
+      client = new MinQuery({
+        keyFile: __dirname + '/fixtures/fake-key.pem',
+        email: 'fake-user@staging.iam.gserviceaccount.com',
+        projectId: 'fake-project'
+      });
+
+      nockBack = nock.back;
+
+      nockBack.setMode('lockdown');
+      nockBack.fixtures = __dirname + '/fixtures';
+    });
+
+    it('works for good request', (done) => {
+      const schema = [
+        {
+          name: 'flavor',
+          type: 'STRING',
+          mode: 'REQUIRED',
+          description: 'Ice cream flavor.'
+        }
+      ];
+
+      nockBack('create-table-good.json', ignoreOauthNockOpts, (nockDone) => {
+        client.createTable('staging', 'faketable', schema).then(() => {
+          nockDone();
+          done();
+        }).catch((err) => {
+          nockDone();
+          done(err);
+        });
+      });
+    });
+
+  });
+
+  describe('#insert', () => {
+
+    let client;
+    let nockBack;
+
+    const ignoreOauthNockOpts = {
+      before: (scope) => {
+        // Because google-oauth-jwt is going to create a unique request
+        // body every time we test, patch nock to treat oauth-looking
+        // requests as the literal body '*'. Fixtures have been patched to
+        // match on that as well.
+        scope.filteringRequestBody = (body) => {
+          if (/grant_type=.*assertion=.*/.test(body)) {
+            return '*';
+          }
+          return body;
+        };
+      }
+    };
+
+    beforeEach(() => {
+      client = new MinQuery({
+        keyFile: __dirname + '/../mikey-test-02.pem',
+        email: 'mikey-test-02@fake-project.iam.gserviceaccount.com',
+        projectId: 'fake-project'
+      });
+
+      nockBack = nock.back;
+
+      nockBack.setMode('record');
+      nockBack.fixtures = __dirname + '/fixtures';
+    });
+
+    it('for all good rows', (done) => {
+      nockBack('insert-good.json', ignoreOauthNockOpts, (nockDone) => {
+        const rows = [
+          { flavor: 'mint' },
+          { flavor: 'bubblegum' },
+          { flavor: 'eggplant' }
+        ];
+
+        client.insert('staging', 'faketable', rows).then((response) => {
+          nockDone();
+          assert.deepEqual(response.body,
+            { kind: 'bigquery#tableDataInsertAllResponse' });
+          done();
+        }).catch((err) => {
+          nockDone();
+          done(err);
+        });
+      });
+    });
+
+    it('with invalid row when ignored', (done) => {
+      nockBack('insert-invalid-row-ignored.json', ignoreOauthNockOpts, (nockDone) => {
+        const rows = [
+          { flavor: 'mint' },
+          { flavor: 'bubblegum' },
+          { topping: 'eggplant' }
+        ];
+
+        const options = {
+          skipInvalidRows: true
+        };
+
+        client.insert('staging', 'faketable', rows, options).then(() => {
+          nockDone();
+          done();
+        }).catch((err) => {
+          nockDone();
+          done(err);
+        });
+      });
+    });
+
+    it('with invalid row when not ignored', (done) => {
+      nockBack('insert-invalid-row-rejected.json', ignoreOauthNockOpts, (nockDone) => {
+        const rows = [
+          { flavor: 'mint' },
+          { flavor: 'bubblegum' },
+          { topping: 'eggplant' }
+        ];
+
+        const options = {
+          skipInvalidRows: false
+        };
+
+        client.insert('staging', 'faketable', rows, options).then(() => {
+          nockDone();
+          done();
+        }).catch((err) => {
+          nockDone();
+          done(err);
+        });
+      });
+    });
+
+    it('with invalid value when ignored', (done) => {
+      nockBack('insert-invalid-value-ignored.json', ignoreOauthNockOpts, (nockDone) => {
+        const rows = [
+          { flavor: 'mint' },
+          { flavor: 'bubblegum' },
+          { flavor: 'eggplant', topping: 'cilantro' }
+        ];
+
+        const options = {
+          ignoreUnknownValues: true
+        };
+
+        client.insert('staging', 'faketable', rows, options).then(() => {
+          nockDone();
+          done();
+        }).catch((err) => {
+          nockDone();
+          done(err);
+        });
+      });
+    });
+
+    it('with invalid value when not ignored', (done) => {
+      nockBack('insert-invalid-value-rejected.json', ignoreOauthNockOpts, (nockDone) => {
+        const rows = [
+          { flavor: 'mint' },
+          { flavor: 'bubblegum' },
+          { flavor: 'eggplant', topping: 'cilantro' }
+        ];
+
+        const options = {
+          ignoreUnknownValues: false
+        };
+
+        client.insert('staging', 'faketable', rows, options).then(() => {
+          nockDone();
+          done();
+        }).catch((err) => {
+          nockDone();
+          done(err);
+        });
+      });
+    });
+
+  });
+
+});
